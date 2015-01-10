af2b16a1df6f32a77e061733531dbb167695a4e6
Cms <cmspixelpilot@yahoo.com>
2015-01-10 19:38:33 +0100

    Turn off INFO level messages to shut up the TTC

--- PixelRun/gdb_run_noRU.sh
+++ PixelRun/gdb_run_noRU.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gdb --args /opt/xdaq/bin/xdaq.exe -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/ConfigurationNoRU.xml -z pixel
+gdb --args /opt/xdaq/bin/xdaq.exe -l WARN -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/ConfigurationNoRU.xml -z pixel
 
--- PixelRun/run_noRU.sh
+++ PixelRun/run_noRU.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 DTSTAMP=`awk "BEGIN{print strftime(\"%y%b%d_%H%M\");}" `
 echo $DTSTAMP
-${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/ConfigurationNoRU.xml 2>&1 -z pixel | tee log.$DTSTAMP
-
+echo
+echo JMT turned off INFO level messages because the TTCciSupervisor is way too chatty...
+echo
+${XDAQ_ROOT}/bin/xdaq.sh -l WARN -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/ConfigurationNoRU.xml 2>&1 -z pixel | tee log.$DTSTAMP
