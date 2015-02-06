38ba5d32d88cde0775a6c0b3cabd706cf9728341
Cms <cmspixelpilot@yahoo.com>
2015-02-06 19:41:16 +0100

    print rocs at first time

--- CalibFormats/SiPixelObjects/src/PixelCalibConfiguration.cc
+++ CalibFormats/SiPixelObjects/src/PixelCalibConfiguration.cc
@@ -951,7 +951,9 @@ void PixelCalibConfiguration::nextFECState(std::map<unsigned int, PixelFECConfig
 
   if (rocInfo_.size()==0){
     //here we will do some initialization...
+    std::cout << "first time, these rocs in the scan:\n";
     for(unsigned int i=0;i<rocs_.size();i++){
+      std::cout << rocs_[i] << "\n";
       const PixelHdwAddress* hdwadd=trans->getHdwAddress(rocs_[i]);
       PixelROCInfo rocInfo;
       rocInfo.use_=true;
