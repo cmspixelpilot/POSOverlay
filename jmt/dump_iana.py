995e3bf883b1e3310397620cb26c647af2d45f9b
Cms <cmspixelpilot@yahoo.com>
2015-02-23 02:40:49 +0100

    dumpiana.py

---
+++ jmt/dump_iana.py
@@ -0,0 +1,42 @@
+import sys, os
+from pprint import pprint
+from JMTTools import *
+from JMTROOTTools import *
+set_style()
+
+run = run_from_argv()
+run_dir = run_dir(run)
+in_fn = os.path.join(run_dir, 'Iana.root')
+if not os.path.isfile(in_fn):
+    raise RuntimeError('no file at %s' % in_fn)
+out_dir = os.path.join(run_dir, 'dump_iana')
+os.system('mkdir -p %s' % out_dir)
+
+f = ROOT.TFile(in_fn)
+
+dirs = [
+    'Pilt/Pilt_BmO/Pilt_BmO_D3/Pilt_BmO_D3_BLD10/Pilt_BmO_D3_BLD10_PNL1/Pilt_BmO_D3_BLD10_PNL1_PLQ1',
+    'Pilt/Pilt_BmO/Pilt_BmO_D3/Pilt_BmO_D3_BLD10/Pilt_BmO_D3_BLD10_PNL2/Pilt_BmO_D3_BLD10_PNL2_PLQ1',
+    'Pilt/Pilt_BmO/Pilt_BmO_D3/Pilt_BmO_D3_BLD11/Pilt_BmO_D3_BLD11_PNL1/Pilt_BmO_D3_BLD11_PNL1_PLQ1',
+    'Pilt/Pilt_BmO/Pilt_BmO_D3/Pilt_BmO_D3_BLD11/Pilt_BmO_D3_BLD11_PNL2/Pilt_BmO_D3_BLD11_PNL2_PLQ1',
+    'Pilt/Pilt_BmI/Pilt_BmI_D3/Pilt_BmI_D3_BLD2/Pilt_BmI_D3_BLD2_PNL1/Pilt_BmI_D3_BLD2_PNL1_PLQ1',
+    'Pilt/Pilt_BmI/Pilt_BmI_D3/Pilt_BmI_D3_BLD2/Pilt_BmI_D3_BLD2_PNL2/Pilt_BmI_D3_BLD2_PNL2_PLQ1',
+    'Pilt/Pilt_BmI/Pilt_BmI_D3/Pilt_BmI_D3_BLD3/Pilt_BmI_D3_BLD3_PNL1/Pilt_BmI_D3_BLD3_PNL1_PLQ1',
+    'Pilt/Pilt_BmI/Pilt_BmI_D3/Pilt_BmI_D3_BLD3/Pilt_BmI_D3_BLD3_PNL2/Pilt_BmI_D3_BLD3_PNL2_PLQ1',
+    ]
+
+for d in dirs:
+    if not f.Get(d):
+        continue
+    for ikey, key in enumerate(f.Get(d).GetListOfKeys()):
+        canvas = key.ReadObj()
+        canvas.SaveAs(os.path.join(out_dir, canvas.GetName() + '.png'))
+
+if 'scp' in sys.argv:
+    remote_dir = 'public_html/qwer/dump_iana/%i' % run
+    cmd = 'ssh jmt46@lnx201.lns.cornell.edu "mkdir -p %s"' % remote_dir
+    print cmd
+    os.system(cmd)
+    cmd = 'scp -r %s/* jmt46@lnx201.lns.cornell.edu:%s' % (out_dir, remote_dir)
+    print cmd
+    os.system(cmd)
