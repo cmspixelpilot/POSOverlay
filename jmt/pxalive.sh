fbcf893db0356ed90be0578dd4d8bdbc6f28dd6e
Cms <cmspixelpilot@yahoo.com>
2015-02-25 03:31:59 +0100

    put log in Logs dir

--- jmt/pxalive.sh
+++ jmt/pxalive.sh
@@ -1,2 +1,2 @@
 RUN=$1
-echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN | gzip > pixelalive.run${RUN}.log.gz
+echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN | gzip > ~/Logs/pixelalive.run${RUN}.log.gz
