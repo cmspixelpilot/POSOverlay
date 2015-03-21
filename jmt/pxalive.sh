55171af0f76d48901fafc0ce271674461982f732
Cms <cmspixelpilot@yahoo.com>
2015-03-21 17:52:27 +0100

    echo the log fn

--- jmt/pxalive.sh
+++ jmt/pxalive.sh
@@ -1,2 +1,5 @@
 RUN=$1
-echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN | gzip > ~/Logs/pixelalive.run${RUN}.log.gz
+LOGFN=~/Logs/pixelalive.run${RUN}.log.gz
+echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN | gzip > $LOGFN
+echo $LOGFN
+
