50cf8918aa48137e865b7fbeb2359c7ccef50c28
Cms <cmspixelpilot@yahoo.com>
2015-06-02 21:28:47 +0200

    Follow pxalive.sh

--- jmt/scurve.sh
+++ jmt/scurve.sh
@@ -1,2 +1,5 @@
 RUN=$1
-echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe SCurve $RUN | gzip > ~/Logs/scurve.run${RUN}.log.gz
+LOGFN=~/Logs/scurve.run${RUN}.log.gz
+echo q | /nfshome0/pixelpilot/build/TriDAS/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe SCurve $RUN | gzip > $LOGFN
+echo $LOGFN
+
