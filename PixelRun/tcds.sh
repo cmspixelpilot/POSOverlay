1c8700a37371feacdecdd071164426106cd93d5b
Cms <cmspixelpilot@yahoo.com>
2015-06-02 21:54:21 +0200

    Change around xml names, and TCDS script

---
+++ PixelRun/tcds.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+DTSTAMP=`awk "BEGIN{print strftime(\"%y%b%d_%H%M\");}" `
+LOGFN=Logs/log.$DTSTAMP
+rm -f lastlog
+ln -s $LOGFN lastlog
+echo $LOGFN
+echo
+${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/ConfigurationNoRU_Pilot_TCDS.xml 2>&1 -z pixel | tee $LOGFN
