0fea674e90edd2874bd8c000dd4bf0558b29b930
Cms <cmspixelpilot@yahoo.com>
2015-06-05 22:01:46 +0200

    Use TCDS version of xml

--- PixelRun/run_noRU.sh
+++ PixelRun/run_noRU.sh
@@ -7,4 +7,4 @@ echo $LOGFN
 echo
 echo JMT turned off INFO level messages because the TTCciSupervisor is way too chatty...
 echo
-${XDAQ_ROOT}/bin/xdaq.sh -l WARN -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilot_TTC.xml 2>&1 -z pixel | tee $LOGFN
+${XDAQ_ROOT}/bin/xdaq.sh -l WARN -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilot_TCDS.xml 2>&1 -z pixel | tee $LOGFN
