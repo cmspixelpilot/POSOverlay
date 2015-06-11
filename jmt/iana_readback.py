7424d748672bb44b86cfd773ed1f949ee8da7c73
Cms <cmspixelpilot@yahoo.com>
2015-06-11 21:39:11 +0200

    fix datum repr; separate plots for adc and caen iana vs vana; slopes histos.

--- jmt/iana_readback.py
+++ jmt/iana_readback.py
@@ -26,7 +26,8 @@ class datum:
         self.vbg = int(vbg)
         self.iana = int(iana)
     def __repr__(self):
-        return 'datum(%r)' % ((self.dac_vana, self.caen_iana, self.vd, self.va, self.vana, self.vbg, self.iana))
+        x = (self.dac_vana, self.caen_iana, self.vd, self.va, self.vana, self.vbg, self.iana)
+        return 'datum' + repr(x)
 
 by_roc = defaultdict(list)
 
@@ -67,37 +68,57 @@ def _key(x):
     return a, int(b)
 by_roc.sort(key=_key)
 
-run = 874
+run = 908
 out_dir = '/nfshome0/pixelpilot/build/TriDAS/pixel/PixelRun/Runs/Run_0/Run_%i/iana_readback' % run
 os.system('mkdir -p %s' % out_dir)
 
-c = ROOT.TCanvas('c', '', 2000, 2000)
-c.Divide(4,4)
-cache = []
-for iroc, (roc, data) in enumerate(by_roc):
-    c.cd(iroc % 16 + 1)
-    data.sort(key=lambda d: d.dac_vana)
-    iana_0 = data[0].iana
-    x = arr(d.dac_vana for d in data)
-    y = arr(d.iana - iana_0 for d in data)
-    e = arr(1 for d in data)
-    g = ROOT.TGraphErrors(len(data), x, y, e, e)
-    g.SetTitle('%s;Vana (DAC units);Iana (ADC units)' % roc)
-    f = ROOT.TF1('f', 'pol1', 25, 200)
-    g.Fit(f, 'QR')
-    g.Draw('AP')
-    c.Update()
-    stats = g.GetListOfFunctions().At(1)
-    assert stats.GetName() == 'stats'
-    stats.SetX1NDC(0.141)
-    stats.SetY1NDC(0.646)
-    stats.SetX2NDC(0.540)
-    stats.SetY2NDC(0.862)
-    cache.append((g,f))
-    if iroc % 16 == 15:
-        c.cd(0)
-        c.SaveAs(os.path.join(out_dir, roc.split('_ROC')[0] + '.png'))
-    
+for kind in 'adc caen'.split():
+    c = ROOT.TCanvas('c', '', 2000, 2000)
+    c.Divide(4,4)
+    cache = []
+    if kind == 'adc':
+        h_slopes = ROOT.TH1F('h_slopes_adc', '', 100, 0.1, 2)
+    else:
+        h_slopes = ROOT.TH1F('h_slopes_caen', '', 100, 0., 0.001)
+
+    for iroc, (roc, data) in enumerate(by_roc):
+        c.cd(iroc % 16 + 1)
+        data.sort(key=lambda d: d.dac_vana)
+        x = arr(d.dac_vana for d in data)
+        if kind == 'adc':
+            iana_0 = data[0].iana
+            y = arr(d.iana - iana_0 for d in data)
+            e = arr(1 for d in data)
+        else:
+            caen_iana_0 = data[0].caen_iana
+            y = arr(d.caen_iana - caen_iana_0 for d in data)
+            e = arr(0.005 for d in data)
+
+        g = ROOT.TGraphErrors(len(data), x, y, e, e)
+        if kind == 'adc':
+            g.SetTitle('%s;Vana (DAC units);Iana (ADC units)' % roc)
+        else:
+            g.SetTitle('%s;Vana (DAC units);Iana (A from CAEN)' % roc)
+
+        f = ROOT.TF1('f', 'pol1', 25, 200)
+        g.Fit(f, 'QR')
+        if kind == 'adc' or 'BmI' in roc:
+            h_slopes.Fill(f.GetParameter(1))
+        g.Draw('AP')
+        c.Update()
+        stats = g.GetListOfFunctions().At(1)
+        assert stats.GetName() == 'stats'
+        stats.SetX1NDC(0.141)
+        stats.SetY1NDC(0.646)
+        stats.SetX2NDC(0.540)
+        stats.SetY2NDC(0.862)
+        cache.append((g,f))
+        if iroc % 16 == 15:
+            c.cd(0)
+            c.SaveAs(os.path.join(out_dir, kind + '_' + roc.split('_ROC')[0] + '.png'))
+    c = ROOT.TCanvas('c', '', 600, 600)
+    h_slopes.Draw()
+    c.SaveAs(os.path.join(out_dir, kind + '_slopes.png'))
 
 if 'scp' in sys.argv:
     remote_dir = 'public_html/qwer/iana_readback/%i' % run
