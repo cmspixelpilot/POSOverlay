bd44e2886083e0bd7a2436deaa0d2859713ec7e3
Cms <cmspixelpilot@yahoo.com>
2015-08-19 02:57:00 +0200

    well well well

--- jmt/dump_tbmdelay.py
+++ jmt/dump_tbmdelay.py
@@ -82,11 +82,11 @@ def analyze_abdel(f, fifo, chip, tbmh_req, tbmt_req, roch_req, wpix_req, rpix_re
     htbmh = f.Get('TBMBDelay_v_TBMADelay_F%i%inTBMHeader%s'  % (fifo, chip, s))
     htbmt = f.Get('TBMBDelay_v_TBMADelay_F%i%inTBMTrailer%s' % (fifo, chip, s))
     hroch = f.Get('TBMBDelay_v_TBMADelay_F%i%inROCHeaders'   % (fifo, chip))
-    hwpix = f.Get('TBMBDelay_v_TBMADelay_F2%iwrongPix' % chip) if fifo == 2 else None
-    hrpix = f.Get('TBMBDelay_v_TBMADelay_F2%irightPix' % chip) if fifo == 2 else None
-    hdang = f.Get('TBMBDelay_v_TBMADelay_F2%idangling' % chip) if fifo == 2 else None
-    if any(not h for h in (htbmh, htbmt, hroch)) or (fifo == 2 and any(not h for h in (hwpix, hrpix, hdang))):
-        print (htbmh, htbmt, hroch, hwpix, hrpix, hdang)
+    hwpix = f.Get('TBMBDelay_v_TBMADelay_F%i%iwrongPix' % (fifo, chip))
+    hrpix = f.Get('TBMBDelay_v_TBMADelay_F%i%irightPix' % (fifo, chip))
+    hdang = f.Get('TBMBDelay_v_TBMADelay_F%i%idangling' % (fifo, chip)) if fifo == 2 else None
+    if any(not h for h in (htbmh, htbmt, hroch, hwpix, hrpix)) or (fifo == 2 and hdang is None):
+        pprint((htbmh, htbmt, hroch, hwpix, hrpix, hdang))
         return None
 
     xax, yax = htbmh.GetXaxis(), htbmh.GetYaxis()
@@ -94,43 +94,54 @@ def analyze_abdel(f, fifo, chip, tbmh_req, tbmt_req, roch_req, wpix_req, rpix_re
     h = ROOT.TH2F('FIFO%i%iok_%i_%i_%i' % (fifo, chip, tbmh_req, tbmt_req, roch_req), '', nbx, xax.GetBinLowEdge(1), xax.GetBinLowEdge(nbx+1), nby, yax.GetBinLowEdge(1), yax.GetBinLowEdge(nby+1))
     h.SetStats(0)
 
+    print '%5s %5s %5s %5s %5s %5s %5s %5s' % tuple('vx vy ntbmh ntbmt nroch nwpix nrpix ndang'.split())
     for ix in xrange(1, nbx+1):
         vx = xax.GetBinLowEdge(ix)
         for iy in xrange(1, nby+1):
             vy = yax.GetBinLowEdge(iy)
 
+            ntbmh = htbmh.GetBinContent(ix, iy)
+            ntbmt = htbmt.GetBinContent(ix, iy)
+            nroch = hroch.GetBinContent(ix, iy)
+            nwpix = hwpix.GetBinContent(ix, iy)
+            nrpix = hrpix.GetBinContent(ix, iy)
+            ndang = hdang.GetBinContent(ix, iy) if fifo == 2 else -1
+
             tbmh_ok = tbmh_req == -1 or htbmh.GetBinContent(ix, iy) == tbmh_req
             tbmt_ok = tbmt_req == -1 or htbmt.GetBinContent(ix, iy) == tbmt_req
             roch_ok = roch_req == -1 or hroch.GetBinContent(ix, iy) == roch_req
-            wpix_ok = fifo != 2 or wpix_req == -1 or hwpix.GetBinContent(ix, iy) == wpix_req
-            rpix_ok = fifo != 2 or rpix_req == -1 or hrpix.GetBinContent(ix, iy) == rpix_req
+            wpix_ok = wpix_req == -1 or hwpix.GetBinContent(ix, iy) == wpix_req
+            rpix_ok = rpix_req == -1 or hrpix.GetBinContent(ix, iy) == rpix_req
             dang_ok = fifo != 2 or dang_req == -1 or hdang.GetBinContent(ix, iy) == dang_req
-            if tbmh_ok and tbmt_ok and roch_ok and wpix_ok and rpix_ok and dang_ok:
+            ok = tbmh_ok and tbmt_ok and roch_ok and wpix_ok and rpix_ok and dang_ok
+            print '%5i %5i %5i %5i %5i %5i %5i %5i %s' % (vx, vy, ntbmh, ntbmt, nroch, nwpix, nrpix, ndang, ('OK' if ok else ''))
+            if ok:
                 h.Fill(vx, vy)
 
     return h
 
-for ikey, key in enumerate(keys):
-    obj = key.ReadObj()
-    if issubclass(type(obj), ROOT.TH1):
-        h = obj
-        name = h.GetName()
-        h.SetStats(0)
-        if '_v_' in name:
-            h.Draw('colz')
-        else:
-            h.Draw()
-        sv(ikey, h)
-        sv(ikey, h, 'root')
+if 0:
+    for ikey, key in enumerate(keys):
+        obj = key.ReadObj()
+        if issubclass(type(obj), ROOT.TH1):
+            h = obj
+            name = h.GetName()
+            h.SetStats(0)
+            if '_v_' in name:
+                h.Draw('colz')
+            else:
+                h.Draw()
+            sv(ikey, h)
+            sv(ikey, h, 'root')
 
-        if name.startswith('TBMPLL_') and '_v_' not in name:
-            h2 = unflatten_pll(h)
-            h2.Draw('colz text')
-            sv(ikey, h2)
+            if name.startswith('TBMPLL_') and '_v_' not in name:
+                h2 = unflatten_pll(h)
+                h2.Draw('colz text')
+                sv(ikey, h2)
 
-for fifo in (2,):
-    for chip in (5,):
-        h = analyze_abdel(f, fifo, chip, tbmh_req=3, tbmt_req=3, roch_req=3*8, wpix_req=0, rpix_req=3*7, dang_req=0)
+for fifo in (1,2):
+    for chip in (1,):
+        h = analyze_abdel(f, fifo, chip, tbmh_req=4, tbmt_req=4, roch_req=8*4, wpix_req=0, rpix_req=2*4*8, dang_req=0)
         if h is not None:
             h.Draw('colz')
             sv(999, h)
