33e36e194dd11aa84a8eb067a295fe4e154c4668
Cms <cmspixelpilot@yahoo.com>
2015-09-18 20:47:44 +0200

    Naming scheme from Benedikt

--- PixelRun/run_noRU_noDCS.sh
+++ PixelRun/run_noRU_noDCS.sh
@@ -7,4 +7,4 @@ echo $LOGFN
 echo
 #echo JMT turned off INFO level messages because the TTCciSupervisor is way too chatty...
 #echo
-${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilot_AIO_TCDS.xml 2>&1 -z pixel | tee $LOGFN
+${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS.xml 2>&1 -z pixel | tee $LOGFN
