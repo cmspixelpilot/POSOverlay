977e60b7481dd4c020641c8b70ff773f6896aedc
Cms <cmspixelpilot@yahoo.com>
2016-03-17 10:28:23 +0100

    PixelDCSInterface svn trunk merge

--- PixelDCSInterface/src/common/version.cc
+++ PixelDCSInterface/src/common/version.cc
@@ -1,32 +1,24 @@
-// $Id: version.cc,v 1.3 2007/09/26 13:31:32 veelken Exp $
-
-/*************************************************************************
- * Auxiliary header to define XDAQ internal versioning information       *
- * about PixelDCSInterface package in the shared object library         *
- * dynamically loaded by XDAQ at run-time                                *
- *                                                                       *
- * Author: Christian Veelken, UC Davis			 	         *
- *                                                                       *
- * Last update: $Date: 2007/09/26 13:31:32 $ (UTC)                       *
- *          by: $Author: veelken $                                       *
- *************************************************************************/
-
-#include "version.h"
 #include "config/version.h"
+#include "xcept/version.h"
+#include "xdaq/version.h"
+#include "PixelDCSInterface/version.h"
 
 GETPACKAGEINFO(PixelDCSInterface)
 
-void PixelDCSInterface::checkPackageDependencies() throw (config::PackageInfo::VersionException)
-{
-  CHECKDEPENDENCY(config);   
+void PixelDCSInterface::checkPackageDependencies() throw (config::PackageInfo::VersionException) {
+
+  CHECKDEPENDENCY(config);
+  CHECKDEPENDENCY(xcept);
+  CHECKDEPENDENCY(xdaq);
+  
 }
 
-std::set<std::string, std::less<std::string> > PixelDCSInterface::getPackageDependencies()
-{
+std::set<std::string, std::less<std::string> > PixelDCSInterface::getPackageDependencies() {
+  
   std::set<std::string, std::less<std::string> > dependencies;
-  
-  ADDDEPENDENCY(dependencies, config); 
-  
+  ADDDEPENDENCY(dependencies,config);
+  ADDDEPENDENCY(dependencies,xcept);
+  ADDDEPENDENCY(dependencies,xdaq);
   return dependencies;
-}	
-	
+  
+}
