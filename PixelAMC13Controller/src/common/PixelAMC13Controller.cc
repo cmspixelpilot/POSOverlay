8a6b7fb1487089dc209a8c4a872504ccd71884fb
Dan Quach <quach.dan@gmail.com>
2016-04-15 18:09:35 -0400

    preparing for multiple bgo command functionality

--- PixelAMC13Controller/src/common/PixelAMC13Controller.cc
+++ PixelAMC13Controller/src/common/PixelAMC13Controller.cc
@@ -15,14 +15,20 @@ XDAQ_INSTANTIATOR_IMPL(PixelAMC13Controller)
 PixelAMC13Controller::PixelAMC13Controller(xdaq::ApplicationStub * s) throw (xdaq::exception::Exception) :xdaq::Application(s) 
 {
   xoap::bind(this, &PixelAMC13Controller::userCommand, "userCommand", XDAQ_NS_URI );
-  
+  xoap::bind(this, &PixelAMC13Controller::Reset, "Reset", XDAQ_NS_URI );
+  xoap::bind(this, &PixelAMC13Controller::Enable, "Enable", XDAQ_NS_URI );
+  xoap::bind(this, &PixelAMC13Controller::Stop, "Stop", XDAQ_NS_URI );
+  xoap::bind(this, &PixelAMC13Controller::Suspend, "Suspend", XDAQ_NS_URI );  
+ 
   getApplicationInfoSpace()->fireItemAvailable("Uri1", &cUri1);
   getApplicationInfoSpace()->fireItemAvailable("AddressT1", &cAddressT1);
   getApplicationInfoSpace()->fireItemAvailable("Uri2", &cUri2);
   getApplicationInfoSpace()->fireItemAvailable("AddressT2", &cAddressT2);
+  getApplicationInfoSpace()->fireItemAvailable("AMCMask", &amcMaskStr);
   getApplicationInfoSpace()->fireItemAvailable("BGORepeat", &bgoRepeat);
   getApplicationInfoSpace()->fireItemAvailable("BGOPrescale", &bgoPrescale);
   getApplicationInfoSpace()->fireItemAvailable("BGOBX", &bgoBX);
+
 }
 
 xoap::MessageReference PixelAMC13Controller::userCommand (xoap::MessageReference msg) throw (xoap::exception::Exception) {
@@ -63,30 +69,85 @@ xoap::MessageReference PixelAMC13Controller::userCommand (xoap::MessageReference
   printf("Parameters are:\n");
   printf("0: %s\n", parameters[0].value_.c_str());
   printf("1: %s\n", parameters[1].value_.c_str());
+  
+  amc13Description = new Amc13Description();
+  amc13Description->setAMCMask(parseAMCMask(amcMaskStr));
 
-  bgoCommand = 0;
-  if (parameters[1].value_ == "CalSync")
-    bgoCommand = 44;
-  if (parameters[1].value_ == "ResetROC")
-    bgoCommand = 28;
-  if (parameters[1].value_ == "ResetTBM")
-    bgoCommand = 20;
+  int calsyncCom = 0x2c;
+  int resetROCCom = 0x1c;
+  int resetTBMCom = 0x14;
+  amc13Description->addBGO(calsyncCom, bgoRepeat, bgoPrescale, bgoBX);
+  amc13Description->addBGO(resetROCCom, bgoRepeat, bgoPrescale, bgoBX);
+  amc13Description->addBGO(resetTBMCom, bgoRepeat, bgoPrescale, bgoBX);  
+  
+  amc13Interface = new Amc13Interface(cUri1, cAddressT1, cUri2, cAddressT2);
+  amc13Interface->setAmc13Description(amc13Description);
+  amc13Interface->ConfigureAmc13();
 
-  Amc13Description* fAmc13 = new Amc13Description();
-  fAmc13->setAMCMask({3});
-  fAmc13->addBGO(bgoCommand, bgoRepeat, bgoPrescale, bgoBX);
+  if (parameters[1].value_ == "CalSync") {
+    amc13Interface->DisableBGO(1);
+    amc13Interface->DisableBGO(2);
+  }
+  if (parameters[1].value_ == "ResetROC") {
+    amc13Interface->DisableBGO(0);
+    amc13Interface->DisableBGO(2);
+  }
+  if (parameters[1].value_ == "ResetTBM") {
+    amc13Interface->DisableBGO(0);
+    amc13Interface->DisableBGO(1);
+  }
 
-  Amc13Interface* fAmc13Interface = new Amc13Interface(cUri1, cAddressT1, cUri2, cAddressT2);
+  amc13Interface->FireBGO();
 
-  fAmc13Interface->setAmc13Description(fAmc13);
-  fAmc13Interface->ConfigureAmc13();  
-  fAmc13Interface->FireBGO();
+  xoap::MessageReference reply = xoap::createMessage();
+  xoap::SOAPEnvelope replyEnvelope = reply->getSOAPPart().getEnvelope();
+  xoap::SOAPName responseName = replyEnvelope.createName( "userTTCciControlResponse", "xdaq", XDAQ_NS_URI);
+  xoap::SOAPBodyElement e = replyEnvelope.getBody().addBodyElement ( responseName );
+  return reply;
+}
 
-  {
-    xoap::MessageReference reply = xoap::createMessage();
-    xoap::SOAPEnvelope envelope = reply->getSOAPPart().getEnvelope();
-    xoap::SOAPName responseName = envelope.createName( "userTTCciControlResponse", "xdaq", XDAQ_NS_URI);
-    xoap::SOAPBodyElement e = envelope.getBody().addBodyElement ( responseName );
-    return reply;
-  }	       
+xoap::MessageReference PixelAMC13Controller::Reset (xoap::MessageReference msg) throw (xoap::exception::Exception) {
+  xoap::MessageReference reply = xoap::createMessage();
+  xoap::SOAPEnvelope envelope = reply->getSOAPPart().getEnvelope();
+  xoap::SOAPName responseName = envelope.createName( "dummyResponse", "xdaq", XDAQ_NS_URI);
+  xoap::SOAPBodyElement e = envelope.getBody().addBodyElement ( responseName );
+  return reply;
+}
+
+xoap::MessageReference PixelAMC13Controller::Enable (xoap::MessageReference msg) throw (xoap::exception::Exception) {
+  xoap::MessageReference reply = xoap::createMessage();
+  xoap::SOAPEnvelope envelope = reply->getSOAPPart().getEnvelope();
+  xoap::SOAPName responseName = envelope.createName( "dummyResponse", "xdaq", XDAQ_NS_URI);
+  xoap::SOAPBodyElement e = envelope.getBody().addBodyElement ( responseName );
+  return reply;
+}
+
+xoap::MessageReference PixelAMC13Controller::Stop (xoap::MessageReference msg) throw (xoap::exception::Exception) {
+  xoap::MessageReference reply = xoap::createMessage();
+  xoap::SOAPEnvelope envelope = reply->getSOAPPart().getEnvelope();
+  xoap::SOAPName responseName = envelope.createName( "dummyResponse", "xdaq", XDAQ_NS_URI);
+  xoap::SOAPBodyElement e = envelope.getBody().addBodyElement ( responseName );
+  return reply;
+}
+
+xoap::MessageReference PixelAMC13Controller::Suspend (xoap::MessageReference msg) throw (xoap::exception::Exception) {
+  xoap::MessageReference reply = xoap::createMessage();
+  xoap::SOAPEnvelope envelope = reply->getSOAPPart().getEnvelope();
+  xoap::SOAPName responseName = envelope.createName( "dummyResponse", "xdaq", XDAQ_NS_URI);
+  xoap::SOAPBodyElement e = envelope.getBody().addBodyElement ( responseName );
+  return reply;
+}
+
+
+std::vector<int> PixelAMC13Controller::parseAMCMask(xdata::String maskStr) {
+  std::vector<int> amcVec;
+
+  std::string cList = maskStr;
+  std::string ctoken;
+  std::stringstream cStr(cList);
+
+  while (std::getline(cStr, ctoken, ',')) {
+    amcVec.push_back(convertAnyInt( ctoken.c_str() ));
+  }
+  return amcVec;
 }
