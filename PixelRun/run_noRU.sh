95cb25392b84323edffe42532a534c5500d38c65
Cms <cmspixelpilot@yahoo.com>
2016-04-20 17:35:01 +0200

    Use profile from -z pixel

--- PixelRun/run_noRU.sh
+++ PixelRun/run_noRU.sh
@@ -6,8 +6,8 @@ ln -s $LOGFN lastlog
 echo $LOGFN
 echo
 if [ "$1" == "gdb" ]; then
-    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml -z pixel
+    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -z pixel -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml
 else
-    ${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml 2>&1 -z pixel | tee $LOGFN
+    ${XDAQ_ROOT}/bin/xdaq.sh -z pixel -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml 2>&1 | tee $LOGFN
 fi
 
