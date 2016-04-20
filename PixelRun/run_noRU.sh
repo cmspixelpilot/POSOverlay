16579e5a73679374eb025d2ab762fb2c5f5ed3d5
Cms <cmspixelpilot@yahoo.com>
2016-04-20 03:12:15 +0200

    gdb in arg

--- PixelRun/run_noRU.sh
+++ PixelRun/run_noRU.sh
@@ -5,6 +5,9 @@ rm -f lastlog
 ln -s $LOGFN lastlog
 echo $LOGFN
 echo
-#echo JMT turned off INFO level messages because the TTCciSupervisor is way too chatty...
-#echo
-${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml 2>&1 -z pixel | tee $LOGFN
+if [ "$1" == "gdb" ]; then
+    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml -z pixel
+else
+    ${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml 2>&1 -z pixel | tee $LOGFN
+fi
+
