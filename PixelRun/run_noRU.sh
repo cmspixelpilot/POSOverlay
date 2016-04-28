b4d42e13673e59386feb820c82b2a25d8461ae49
Cms <cmspixelpilot@yahoo.com>
2016-04-28 18:59:57 +0200

    better script

--- PixelRun/run_noRU.sh
+++ PixelRun/run_noRU.sh
@@ -5,9 +5,38 @@ rm -f lastlog
 ln -s $LOGFN lastlog
 echo $LOGFN
 echo
-if [ "$1" == "gdb" ]; then
-    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -z pixel -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml
+
+gdb=0
+dcs=1
+
+while test $# -gt 0
+do
+    case "$1" in
+        --gdb) gdb=1
+            ;;
+        --nodcs) dcs=0
+            ;;
+        --*) echo "bad option $1"
+            ;;
+        *) echo "argument $1"
+            ;;
+    esac
+    shift
+done
+
+cmd='-z pixel -p 1973 -c '
+
+if [ "$dcs" == "1" ]; then
+    cmd="$cmd ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml"
 else
-    ${XDAQ_ROOT}/bin/xdaq.sh -z pixel -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS_DCS.xml 2>&1 | tee $LOGFN
+    cmd="$cmd ${BUILD_HOME}/pixel/XDAQConfiguration/XDAQ_ConfigurationPilotPix_AIO_TCDS.xml"
 fi
 
+if [ "$gdb" == "0" ]; then
+    cmd="${XDAQ_ROOT}/bin/xdaq.sh $cmd"
+else
+    cmd="gdb --args ${XDAQ_ROOT}/bin/xdaq.exe $cmd"
+fi
+
+echo $cmd
+$cmd
