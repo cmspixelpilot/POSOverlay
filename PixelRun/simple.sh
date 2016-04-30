938f7582db3450868745ecad69ac907485426fc4
FNAL test <fnaltest@fnal.gov>
2016-04-30 11:35:00 -0500

    Don't use Profile

--- PixelRun/simple.sh
+++ PixelRun/simple.sh
@@ -8,8 +8,8 @@ echo
 #echo JMT turned off INFO level messages because the TTCciSupervisor is way too chatty...
 #echo
 if [ "$1" == "gdb" ]; then
-    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/simple.xml -z pixel
+    gdb --args ${XDAQ_ROOT}/bin/xdaq.exe -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/simple.xml -z pixel
 else
-    ${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -e ${BUILD_HOME}/pixel/XDAQConfiguration/Profile.xml -c ${BUILD_HOME}/pixel/XDAQConfiguration/simple.xml -z pixel 2>&1 | tee $LOGFN
+    ${XDAQ_ROOT}/bin/xdaq.sh -p 1973 -c ${BUILD_HOME}/pixel/XDAQConfiguration/simple.xml -z pixel 2>&1 | tee $LOGFN
 fi
 
