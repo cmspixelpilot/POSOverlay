037537f181e80a6fadfadf279fb094f0983ab550
FNAL test <fnaltest@fnal.gov>
2016-04-30 23:45:21 -0500

    Buffered mode works now

--- CalibFormats/SiPixelObjects/src/PixelDACSettings.cc
+++ CalibFormats/SiPixelObjects/src/PixelDACSettings.cc
@@ -547,7 +547,7 @@ void PixelDACSettings::generateConfiguration(PixelFECConfigInterface* pixelFEC,
 					     PixelNameTranslation* trans, PixelDetectorConfig* detconfig, bool HVon) const{
   std::cout << "PixelDACSettings::generateConfiguration HVon=" << int(HVon) << " for " << dacsettings_.size() << " rocs including " << dacsettings_[0].getROCName() << std::endl;
 
-  bool bufferData=false; 
+  bool bufferData=true;
 
   std::vector<unsigned int> dacs;
 
