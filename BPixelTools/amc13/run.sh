6d83263a735de4b0cbd61c06a7f0904e34b8ca6e
FNAL test <fnaltest@fnal.gov>
2016-05-02 14:06:24 -0500

    Option for no reset

--- BPixelTools/amc13/run.sh
+++ BPixelTools/amc13/run.sh
@@ -1,1 +1,6 @@
-/opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13
+if [ "$1" == "noreset" ]; then
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml
+else
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13
+fi
+
