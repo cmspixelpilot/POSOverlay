1248eccfd138669fe0c276d55dd573f79579b58a
FNAL test <fnaltest@fnal.gov>
2016-05-02 14:07:56 -0500

    Get rid of obnoxious xdaq comment

--- PixelAMC13Controller/src/common/PixelAMC13Controller.cc
+++ PixelAMC13Controller/src/common/PixelAMC13Controller.cc
@@ -1,13 +1,3 @@
-/*************************************************************************
- * XDAQ Components for Distributed Data Acquisition                      *
- * Copyright (C) 2000-2009, CERN.			                 *
- * All rights reserved.                                                  *
- * Authors: J. Gutleber and L. Orsini					 *
- *                                                                       *
- * For the licensing terms see LICENSE.		                         *
- * For the list of contributors see CREDITS.   			         *
- *************************************************************************/
-
 #include "PixelAMC13Controller.h"
 
 XDAQ_INSTANTIATOR_IMPL(PixelAMC13Controller)
