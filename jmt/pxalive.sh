0279fbd0ab67b65eb82ee8d358a7ffd7aa3a8a5a
FNAL test <fnaltest@fnal.gov>
2016-05-02 11:17:36 -0500

    Use $POS_LOG_DIR

--- jmt/pxalive.sh
+++ jmt/pxalive.sh
@@ -1,4 +1,4 @@
 RUN=$1
-LOGFN=~/Logs/pixelalive.run${RUN}.log.gz
+LOGFN=${POS_LOG_DIR}/pixelalive.run${RUN}.log.gz
 echo q | ${BUILD_HOME}/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN 2>&1 | gzip > $LOGFN
 echo $LOGFN
