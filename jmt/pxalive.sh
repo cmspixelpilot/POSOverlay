1db93a33ae3ffd2f13533398fe5ac64da3b6edc3
FNAL test <fnaltest@fnal.gov>
2016-05-02 16:55:16 -0500

    Take run number from current directory if possible

--- jmt/pxalive.sh
+++ jmt/pxalive.sh
@@ -1,4 +1,12 @@
 RUN=$1
+if [ "$RUN" == "" ]; then
+    RUN=$(echo $(pwd) | sed 's/.*_//')
+    if ! [[ $RUN =~ $re ]] ; then
+        echo "error: need run number" >&2
+        exit 1
+    fi
+fi
+
 LOGFN=${POS_LOG_DIR}/pixelalive.run${RUN}.log.gz
 echo q | ${BUILD_HOME}/pixel/PixelAnalysisTools/test/bin/linux/x86_64_slc6/PixelAnalysis.exe PixelAlive $RUN 2>&1 | gzip > $LOGFN
 echo $LOGFN
