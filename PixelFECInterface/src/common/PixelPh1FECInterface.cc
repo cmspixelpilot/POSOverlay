064ecc92eb7c43c7fa81f89c3c0641b481e2e7d9
FNAL test <fnaltest@fnal.gov>
2016-05-05 19:34:05 -0500

    max buffer -> 15k

--- PixelFECInterface/src/common/PixelPh1FECInterface.cc
+++ PixelFECInterface/src/common/PixelPh1FECInterface.cc
@@ -44,7 +44,7 @@ fecSlot_(fecSlot)
     //Constructor stuff
     if (PRINT) cout << "PixelPh1FECInterface: "  << "PixelPh1FECInterface Constructor" << endl;
     
-    maxbuffersize_=10000;
+    maxbuffersize_=15000;
     cout << "PixelPh1FECInterface::maxbuffersize_ set to " << maxbuffersize_ << "!!! change this in future\n";
     fecdebug = 0;
     
