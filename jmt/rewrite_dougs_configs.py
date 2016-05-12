72f7d2da24dfdb334c6b47c1051c3747cf97d1f9
FNAL test <fnaltest@fnal.gov>
2016-05-12 15:48:25 -0500

    More flexible

--- jmt/rewrite_dougs_configs.py
+++ jmt/rewrite_dougs_configs.py
@@ -1,30 +1,49 @@
+#!/usr/bin/env python
 
 id2name = {
-5 :'M-G-1-34',
-8 :'M-G-1-36',
-9 :'M-G-1-46',
-2 :'M-G-2-40',
-3 :'M-H-1-13',
-6 :'M-H-2-14',
-1 :'M-H-2-15',
-11:'M-H-2-37',
+1  : 'M-H-2-15',
+2  : 'M-G-2-40',
+3  : 'M-H-1-13',
+4  : 'M-H-1-23',
+5  : 'M-G-1-34',
+6  : 'M-H-2-14',
+7  : 'M-H-2-42',
+8  : 'M-G-1-36',
+9  : 'M-G-1-46',
+10 : 'M-K-1-03',
+11 : 'M-H-2-37',
+12 : 'M-H-2-48',
+13 : 'M-H-2-35',
+14 : 'M-H-1-48',
 }
 
-dacdir = 'dac/5'
-tbmdir = 'tbm/1'
-trimdir = 'trim/2'
+dacdir = 'dac/8'
+tbmdir = 'tbm/3'
+trimdir = 'trim/4'
 
-lastlines = '''WBC: 92
-ChipContReg: 0
-Readback: 12
-'''
+WBC = 92
+Readback = 12
 
+#######
+
+import os
+
+base = os.environ.get('PIXELCONFIGURATIONBASE', '')
+dacdir = os.path.join(base, dacdir)
+tbmdir = os.path.join(base, tbmdir)
+trimdir = os.path.join(base, trimdir)
+
+if not os.path.isdir(base) or any(not os.path.isdir(os.path.join(base, x)) for x in ('dac', 'tbm', 'trim')):
+    raise IOError('PIXELCONFIGURATIONBASE="%s" does not seem to be a pixel config directory' % base)
+for x in (dacdir, tbmdir, trimdir):
+    if os.path.isdir(x):
+        raise IOError('%s already exists' % x)
+
+for x in (dacdir, tbmdir, trimdir):
+    os.mkdir(x)
 
 for modid in xrange(1,15):
-    if id2name.has_key(modid):
-        oldname2 = id2name[modid]
-    else:
-        oldname2 = 'M-H-2-37'
+    oldname2 = id2name[modid]
     oldname = 'FPIX_%s' % oldname2
     olddir = 'TestArea/%s' % oldname2
 
@@ -47,9 +66,9 @@ for modid in xrange(1,15):
             roc = line.split('ROC')[-1]
             line = 'ROC: ' + newname2 + '_ROC' + roc
         elif 'WBC' in line:
-            line = 'WBC: 92\n'
+            line = 'WBC: %s\n' % WBC
         elif 'Readback' in line:
-            line = 'Readback: 12\n'
+            line = 'Readback: %s\n' % Readback
         f.write(line)
     f.close()
 
@@ -75,4 +94,3 @@ for modid in xrange(1,15):
             line = 'ROC: ' + newname2 + '_ROC' + roc
         f.write(line)
     f.close()
-
