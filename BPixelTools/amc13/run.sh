5f50f9b391204c687e4c6bf79d24b4c03989fefe
FNAL test <fnaltest@fnal.gov>
2016-06-27 13:43:27 -0500

    Reset when running with ttcci clock

--- BPixelTools/amc13/run.sh
+++ BPixelTools/amc13/run.sh
@@ -1,6 +1,7 @@
 if [ "$1" == "noreset" ]; then
     /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml
+elif [ "$1" == "ttcci" ]; then
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13_ttcci
 else
     /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13
 fi
-
