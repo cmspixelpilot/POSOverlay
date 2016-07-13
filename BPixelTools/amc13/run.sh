e7cdcff3bba3588117998e4d321c97b90b310b45
Cms <cmspixelpilot@yahoo.com>
2016-07-13 05:31:51 +0200

    untested but better

--- BPixelTools/amc13/run.sh
+++ BPixelTools/amc13/run.sh
@@ -1,7 +1,30 @@
-if [ "$1" == "noreset" ]; then
-    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml
-elif [ "$1" == "ttcci" ]; then
-    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13_ttcci
+xml=amc13.xml
+reset=1
+externaltrig=0
+
+while test $# -gt 0
+do
+    case "$1" in
+        --xml)
+            shift
+            xml=$1
+            ;;
+        --noreset) reset=0
+            ;;
+        --externaltrig) externaltrig=1
+            ;;
+        --*) echo "bad option $1"
+            ;;
+        *) echo "argument $1 not parsed"
+            ;;
+    esac
+    shift
+done
+
+if [ "$reset" != "1" ]; then
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c $xml
+elif [ "$externaltrig" == "1" ]; then
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c $xml -X reset_amc13_externaltrig
 else
-    /opt/cactus/bin/amc13/AMC13Tool2.exe -c amc13.xml -X reset_amc13
+    /opt/cactus/bin/amc13/AMC13Tool2.exe -c $xml -X reset_amc13
 fi
