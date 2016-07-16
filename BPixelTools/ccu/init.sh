88892c3f54f733685c4680473ae6f005370c3d50
FNAL test <fnaltest@fnal.gov>
2016-07-15 21:06:01 -0500

    init.sh reads all the power good and enable bits, and does everything for all 4 ccus

--- BPixelTools/ccu/init.sh
+++ BPixelTools/ccu/init.sh
@@ -2,6 +2,48 @@
 
 cat | (bin/ccu -utca connections.xml -fechardwareid board)  <<EOF
 scanccu
+ccu 0x7b
 i2cr 10 40
+piaChannel 31
+pia get ddr
+pia get data
+piaChannel 32
+pia get ddr
+pia get data
+piaChannel 33
+pia get ddr
+pia get data
+ccu 0x7c
+i2cr 10 40
+piaChannel 31
+pia get ddr
+pia get data
+piaChannel 32
+pia get ddr
+pia get data
+piaChannel 33
+pia get ddr
+pia get data
+ccu 0x7d
+i2cr 10 40
+piaChannel 31
+pia get ddr
+pia get data
+piaChannel 32
+pia get ddr
+pia get data
+piaChannel 33
+pia get ddr
+pia get data
+ccu 0x7e
+i2cr 10 40
+piaChannel 31
+pia get ddr
+pia get data
+piaChannel 32
+pia get ddr
+pia get data
+piaChannel 33
+pia get ddr
 pia get data
 EOF
