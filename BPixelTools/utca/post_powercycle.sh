124f1f065a69556e6c1565c6ab2f6ffe4c3a56ee
FNAL test <fnaltest@fnal.gov>
2016-07-26 16:23:38 -0500

    ips from cmd line

--- BPixelTools/utca/post_powercycle.sh
+++ BPixelTools/utca/post_powercycle.sh
@@ -1,9 +1,7 @@
-todo="pxfec09 fed1294 fed1295 fed1296 fed1297 fed1298"
-todo="fed1294 fed1295 fed1296 fed1297 fed1298"
 
-for ip in $todo; do 
+for ip in $@; do 
     $BUILD_HOME/pixel/PixelUtilities/PixeluTCAUtilities/test/src/common/banger.exe \
         board chtcp-2.0://localhost:10203?target=${ip}:50001 \
-        file://${BUILD_HOME}/pixel/BPixelTools/utca_scripts/address_table.xml \
+        file://${BUILD_HOME}/pixel/BPixelTools/utca/address_table.xml \
         write ctrl.ttc_xpoint_A_out3 0
 done
