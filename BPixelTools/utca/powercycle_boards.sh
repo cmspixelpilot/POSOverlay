d1b76b2bf9b6391fff1690a049a46a94cb4bbd93
FNAL test <fnaltest@fnal.gov>
2016-07-26 16:23:48 -0500

    slots from cmd line

--- BPixelTools/utca/powercycle_boards.sh
+++ BPixelTools/utca/powercycle_boards.sh
@@ -1,10 +1,6 @@
-# obviously this has to expand to multiple crates
+# this doesn't deal with multiple crates
 
-fed_slots="2 4 5 6 7"
-tkfec_slots=9
-pxfec_slots=11
-
-for slot in $fed_slots; do
+for slot in $@; do
     addr=$((0x70 + 2*$slot))
     addrs=$(printf '0x%x' $addr)
     echo ipmitool -H 192.168.1.41 -P '' -B 0 -T 0x82 -b 7 -t $addrs raw 0x30 0xff 0xde 0xad
