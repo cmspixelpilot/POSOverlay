9f75eb1e2c09fb30010f97d76c80fcc59417c65c
FNAL test <fnaltest@fnal.gov>
2016-07-27 11:39:19 -0500

    new banger arg format

--- BPixelTools/utca/post_powercycle.sh
+++ BPixelTools/utca/post_powercycle.sh
@@ -1,7 +1,7 @@
 
 for ip in $@; do 
     $BUILD_HOME/pixel/PixelUtilities/PixeluTCAUtilities/test/src/common/banger.exe \
-        board chtcp-2.0://localhost:10203?target=${ip}:50001 \
-        file://${BUILD_HOME}/pixel/BPixelTools/utca/address_table.xml \
+        chtcp-2.0://localhost:10203?target=${ip}:50001 \
+        ${BUILD_HOME}/pixel/BPixelTools/utca/address_table.xml \
         write ctrl.ttc_xpoint_A_out3 0
 done
