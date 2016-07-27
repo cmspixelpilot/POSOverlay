521b91500c9684d43bfa7fff5d31d19db98288bd
FNAL test <fnaltest@fnal.gov>
2016-07-27 17:07:31 -0500

    path to banger.exe

--- BPixelTools/utca/post_powercycle.sh
+++ BPixelTools/utca/post_powercycle.sh
@@ -1,6 +1,6 @@
 
 for ip in $@; do 
-    $BUILD_HOME/pixel/PixelUtilities/PixeluTCAUtilities/test/src/common/banger.exe \
+    $BUILD_HOME/pixel/bin/banger.exe \
         chtcp-2.0://localhost:10203?target=${ip}:50001 \
         ${BUILD_HOME}/pixel/BPixelTools/utca/address_table.xml \
         write ctrl.ttc_xpoint_A_out3 0
