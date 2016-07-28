7761c378e7d00e069165903e24ad2b9817f4f60c
FNAL test <fnaltest@fnal.gov>
2016-07-27 19:03:34 -0500

    new fec fw

--- BPixelTools/utca/fc7/powerup.sh
+++ BPixelTools/utca/fc7/powerup.sh
@@ -1,5 +1,5 @@
 ./fc7firmwarer.exe -i trkfec -s fc7_tkfec_rarp_en.bit
-./fc7firmwarer.exe -i pxfec09 -s 101_2.bin
+./fc7firmwarer.exe -i pxfec09 -s fc7_top1_0_9.bin
 for x in fed{1294..1300}; do
     ./fc7firmwarer.exe -i $x -s fc7_top_v6.10.5.bin
 done
