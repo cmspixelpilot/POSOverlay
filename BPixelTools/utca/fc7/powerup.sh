ddf61c9a7331e664d90f5062a0ae2932cddca818
FNAL test <fnaltest@fnal.gov>
2016-07-29 10:59:55 -0500

    pxfec09,10; new fed fw

--- BPixelTools/utca/fc7/powerup.sh
+++ BPixelTools/utca/fc7/powerup.sh
@@ -1,5 +1,7 @@
 ./fc7firmwarer.exe -i trkfec -s fc7_tkfec_rarp_en.bit
-./fc7firmwarer.exe -i pxfec09 -s fc7_top1_0_9.bin
+for x in pxfec{09,10}; do
+    ./fc7firmwarer.exe -i $x -s fc7_top1_0_9.bin
+done
 for x in fed{1294..1300}; do
-    ./fc7firmwarer.exe -i $x -s fc7_top_v6.10.5.bin
+    ./fc7firmwarer.exe -i $x -s fc7_top_v6.10.6.bin
 done
