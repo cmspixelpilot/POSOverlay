627fe75d0d7fd41436d141716dbd4ab5babe35c7
FNAL test <fnaltest@fnal.gov>
2016-08-03 18:40:04 -0500

    include ttc reset (just for pxfecs\!)

--- BPixelTools/utca/post_powercycle.sh
+++ BPixelTools/utca/post_powercycle.sh
@@ -1,7 +1,20 @@
 
-for ip in $@; do 
+for ip in pxfec{09,10}; do 
     $BUILD_HOME/pixel/bin/banger.exe \
         chtcp-2.0://localhost:10203?target=${ip}:50001 \
         ${BUILD_HOME}/pixel/BPixelTools/utca/address_table.xml \
         write ctrl.ttc_xpoint_A_out3 0
+
+    $BUILD_HOME/pixel/bin/banger.exe \
+        chtcp-2.0://localhost:10203?target=${ip}:50001 \
+        ${BUILD_HOME}/pixel/PixelFECInterface/dat/address_table.xml \
+        write Board0.TTCrst 1
+
+    sleep 1
+
+    $BUILD_HOME/pixel/bin/banger.exe \
+        chtcp-2.0://localhost:10203?target=${ip}:50001 \
+        ${BUILD_HOME}/pixel/PixelFECInterface/dat/address_table.xml \
+        write Board0.TTCrst 0
 done
+
