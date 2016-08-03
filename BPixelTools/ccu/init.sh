f07d914c16c5e66cd639824c595b301e1a0bfefc
FNAL test <fnaltest@fnal.gov>
2016-08-03 10:34:32 -0500

    channel at top of script

--- BPixelTools/ccu/init.sh
+++ BPixelTools/ccu/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+channel=11
+
 cat | (bin/ccu -utca connections.xml -fechardwareid board)  <<EOF
 scanccu
 ccu 0x7b
-i2cr 10 40
+i2cr $channel 40
 piaChannel 31
 pia get ddr
 pia get data
@@ -14,7 +16,7 @@ piaChannel 33
 pia get ddr
 pia get data
 ccu 0x7c
-i2cr 10 40
+i2cr $channel 40
 piaChannel 31
 pia get ddr
 pia get data
@@ -25,7 +27,7 @@ piaChannel 33
 pia get ddr
 pia get data
 ccu 0x7e
-i2cr 10 40
+i2cr $channel 40
 piaChannel 31
 pia get ddr
 pia get data
@@ -36,7 +38,7 @@ piaChannel 33
 pia get ddr
 pia get data
 ccu 0x7d
-i2cr 10 40
+i2cr $channel 40
 piaChannel 31
 pia get ddr
 pia get data
