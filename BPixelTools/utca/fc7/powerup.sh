25c44b54f196c9df4501637ffd08c931de1549d4
FNAL test <fnaltest@fnal.gov>
2016-08-05 14:37:16 -0500

    don't do tkfec by default

--- BPixelTools/utca/fc7/powerup.sh
+++ BPixelTools/utca/fc7/powerup.sh
@@ -1,4 +1,6 @@
-./fc7firmwarer.exe -i trkfec -s fc7_tkfec_rarp_en.bit
+if [ "$1" == "tkfec" ]; then
+    ./fc7firmwarer.exe -i trkfec -s fc7_tkfec_rarp_en.bit
+fi
 for x in pxfec{09,10}; do
     ./fc7firmwarer.exe -i $x -s fc7_top1_0_9.bin
 done
