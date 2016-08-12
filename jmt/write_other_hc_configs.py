6e3e770c168e89b2dfced68610fb0e3abc5a99cc
FNAL test <fnaltest@fnal.gov>
2016-08-12 12:24:36 -0500

    Only disconnected modules as not OK

--- jmt/write_other_hc_configs.py
+++ jmt/write_other_hc_configs.py
@@ -229,15 +229,16 @@ class doer:
     # and disconnected modules from too-short cables
     def portcardOK(self, pc):
         #return True
-        return '_D%i_' % self.curr_disk in pc and pc != 'FPix_BmI_D1_PRT4'
+        return '_D%i_' % self.curr_disk in pc # and pc == 'FPix_BmI_D3_PRT1'
 
     def moduleOK(self, m):
         #return True
         if m.disk != self.curr_disk:
             return False
-        if m.name in 'FPix_BmI_D1_BLD1_PNL1_RNG2 FPix_BmI_D1_BLD2_PNL1_RNG2 FPix_BmI_D1_BLD3_PNL1_RNG2 FPix_BmI_D1_BLD4_PNL1_RNG2'.split():
-            return False
-        if m.portcard == 'FPix_BmI_D1_PRT4':
+        #if m.portcard != 'FPix_BmI_D3_PRT1':
+        #    return False
+
+        if m.portcard_hj == '3TA' and m.portcard_connection == 7:
             return False
 
         not_connected = {
